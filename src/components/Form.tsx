6727c6393f33d045e81dfb3bbad583cabcfd4b98
Yuri Odilon <moura.n.yuri@gmail.com>
2025-06-26 14:14:58 -0400

    refactor: formulario do RegisterScreen movido para um componente especifico

---
+++ src/components/Form.tsx
@@ -0,0 +1,159 @@
+import React from "react"
+import { Input } from "./Input"
+import { Select } from "./Select"
+import { Button } from "./Button"
+import type {
+    FormValues,
+    Ticket
+} from "@/interfaces"
+import { api } from "@/api/axios"
+import type { AxiosError } from "axios"
+import { toast } from "sonner"
+import { Loader } from "lucide-react"
+
+interface FormProps {
+    setRequestState: React.Dispatch<React.SetStateAction<{
+        ticket: Ticket | null
+        loading: boolean
+        error: string | null
+    }>>
+    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+    loading: boolean
+}
+
+export function Form({ setRequestState, setIsModalOpen, loading }: FormProps) {
+    const [formValues, setFormValues] = React.useState<FormValues>({
+        cpf: '',
+        name: '',
+        services: '',
+        fila: ''
+    })
+    const { cpf, name, services, fila } = formValues
+
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = event.currentTarget
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            [name]: value,
+        }))
+    }
+
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.currentTarget
+        const sanitizedName = name.value
+            .replace(/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/g, '')
+            .replace(/\s{2,}/g, ' ')
+            .trimStart()
+            .toUpperCase()
+
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            name: sanitizedName
+        }))
+    }
+
+    const handleCpfChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const rawValue = event.currentTarget.value.replace(/\D/g, '')
+        const formattedCpf = rawValue
+            .replace(/^(\d{3})(\d)/, '$1.$2')
+            .replace(/^(\d{3})\.(\d{3})(\d)/, '$1.$2.$3')
+            .replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4')
+
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            cpf: formattedCpf,
+        }))
+    }
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setRequestState((prevStates) => ({
+            ...prevStates,
+            loading: true,
+            error: null
+        }))
+        try {
+            const response = await api.post('/generate-ticket', formValues)
+            const { ticket } = response.data
+
+            setRequestState((prevStates) => ({
+                ...prevStates,
+                ticket: ticket,
+            }))
+
+            setIsModalOpen(true)
+            setFormValues(() => ({
+                cpf: '',
+                name: '',
+                services: '',
+                fila: ''
+            }))
+        } catch (error) {
+            const { response } = error as AxiosError<{ message: string }>
+            setRequestState((prevStates) => ({
+                ...prevStates,
+                error: `Erro ao gerar a senha: ${response?.data.message || 'Erro desconhecido'}`
+            }))
+            toast.error('Error', {
+                description: response?.data.message || 'Erro desconhecido'
+            })
+        } finally {
+            setRequestState((prevStates) => ({
+                ...prevStates,
+                loading: false,
+            }))
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} id="form" className='grid gap-10 w-full max-w-md'>
+            <Input
+                id="cpf"
+                label="CPF"
+                maxLength={14}
+                minLength={14}
+                value={cpf}
+                onChange={handleCpfChange}
+                placeholder="000.000.000-00"
+            />
+            <Input
+                id="name"
+                label="Nome"
+                maxLength={50}
+                minLength={25}
+                value={name}
+                onChange={handleNameChange}
+                placeholder="José da Silva Xavier"
+                required
+            />
+            <Select
+                id="services"
+                label="Serviços"
+                value={services}
+                optionLabel='Selecione um Serviço'
+                options={[{ label: 'PAV', value: 'PAV' }, { label: 'RCN', value: 'RCN' }]}
+                onChange={handleSelectChange}
+                required
+            />
+            <Select
+                id="fila"
+                label="Fila"
+                value={fila}
+                optionLabel='Selecione uma Fila'
+                options={[{ label: 'NORMAL', value: 'N' }, { label: 'PREFERENCIAL', value: 'P' }]}
+                onChange={handleSelectChange}
+                required
+            />
+            <Button disabled={loading}>
+                {loading ? (
+                    <span className="flex items-center gap-2">
+                        <Loader className="animate-spin w-4 h-4" />
+                        Gerando...
+                    </span>
+                ) : (
+                    'Gerar Senha'
+                )}
+            </Button>
+        </form>
+    )
+}
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,20 +1,13 @@
 import React from "react"
 import {
-    Input,
-    Select,
     Button,
     Modal,
-    Header
+    Header,
+    Form
 } from "@/components"
 import type {
     Ticket,
-    FormValues
 } from '@/interfaces'
-import { api } from "@/api/axios"
-import type { AxiosError } from "axios"
-import { toast } from "sonner"
-import { Loader } from 'lucide-react'
-
 
 export function RegisterScreen() {
     const [isModalOpen, setIsModalOpen] = React.useState(false)
@@ -27,111 +20,19 @@ export function RegisterScreen() {
         loading: false,
         error: null
     })
-    const [formValues, setFormValues] = React.useState<FormValues>({
-        cpf: '',
-        name: '',
-        services: '',
-        fila: ''
-    })
-
-    const { cpf, name, services, fila } = formValues
     const { ticket, loading } = requestState
-
-    const handleResetForm = () => {
-        setFormValues(() => ({
-            cpf: '',
-            name: '',
-            services: '',
-            fila: ''
-        }))
-        setIsModalOpen(false)
-    }
-
-    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const { name, value } = event.currentTarget
-
-        setFormValues((prevValues) => ({
-            ...prevValues,
-            [name]: value,
-        }))
-    }
-
-    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const name = event.currentTarget
-
-        const sanitizedName = name.value
-            .replace(/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/g, '')
-            .replace(/\s{2,}/g, ' ')
-            .trimStart()
-            .toUpperCase()
-
-        setFormValues((prevValues) => ({
-            ...prevValues,
-            name: sanitizedName
-        }))
-    }
-
-    const handleCpfChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const rawValue = event.currentTarget.value.replace(/\D/g, '')
-
-        const formattedCpf = rawValue
-            .replace(/^(\d{3})(\d)/, '$1.$2')
-            .replace(/^(\d{3})\.(\d{3})(\d)/, '$1.$2.$3')
-            .replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4')
-
-        setFormValues((prevValues) => ({
-            ...prevValues,
-            cpf: formattedCpf,
-        }))
-    }
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-
-        setRequestState((prevStates) => ({
-            ...prevStates,
-            loading: true,
-            error: null
-        }))
-
-        try {
-            const response = await api.post('/generate-ticket', formValues)
-            const { ticket } = response.data
-
-            setRequestState((prevStates) => ({
-                ...prevStates,
-                ticket: ticket,
-            }))
-
-            setIsModalOpen(true)
-        } catch (error) {
-            const { response } = error as AxiosError<{ message: string }>
-            setRequestState((prevStates) => ({
-                ...prevStates,
-                error: `Erro ao gerar a senha: ${response?.data.message || 'Erro desconhecido'}`
-            }))
-            toast.error('Error', {
-                description: response?.data.message || 'Erro desconhecido'
-            })
-        } finally {
-            setRequestState((prevStates) => ({
-                ...prevStates,
-                loading: false,
-            }))
-        }
-    }
 
     return (
         <>
             {
                 isModalOpen && (
-                    <Modal onClick={handleResetForm}>
+                    <Modal onClick={() => setIsModalOpen(false)}>
                         <div className="grid justify-items-center">
                             <span className="text-xl pb-2">Sua senha: </span>
                             <h1 className="text-2xl font-semibold">{ticket?.name}</h1>
                             <h2 className="text-2xl font-semibold">{ticket?.ticket_number}</h2>
                         </div>
-                        <Button onClick={handleResetForm}>
+                        <Button onClick={() => setIsModalOpen(false)}>
                             Finalizar
                         </Button>
                     </Modal>
@@ -145,59 +46,11 @@ export function RegisterScreen() {
                     <p className="text-base font-medium text-gray-600">Preencha os campos abaixo para gerar uma senha para o seu Atendimento</p>
                 </section>
 
-                <form onSubmit={handleSubmit} id="form" className='grid gap-10 w-full max-w-md'>
-                    <Input
-                        id="cpf"
-                        label="CPF"
-                        maxLength={14}
-                        minLength={14}
-                        value={cpf}
-                        onChange={handleCpfChange}
-                        placeholder="000.000.000-00"
-                    />
-
-                    <Input
-                        id="name"
-                        label="Nome"
-                        maxLength={50}
-                        minLength={25}
-                        value={name}
-                        onChange={handleNameChange}
-                        placeholder="José da Silva Xavier"
-                        required
-                    />
-
-                    <Select
-                        id="services"
-                        label="Serviços"
-                        value={services}
-                        optionLabel='Selecione um Serviço'
-                        options={[{ label: 'PAV', value: 'PAV' }, { label: 'RCN', value: 'RCN' }]}
-                        onChange={handleSelectChange}
-                        required
-                    />
-
-                    <Select
-                        id="fila"
-                        label="Fila"
-                        value={fila}
-                        optionLabel='Selecione uma Fila'
-                        options={[{ label: 'NORMAL', value: 'N' }, { label: 'PREFERENCIAL', value: 'P' }]}
-                        onChange={handleSelectChange}
-                        required
-                    />
-
-                    <Button disabled={loading}>
-                        {loading ? (
-                            <span className="flex items-center gap-2">
-                                <Loader className="animate-spin w-4 h-4" />
-                                Gerando...
-                            </span>
-                        ) : (
-                            'Gerar Senha'
-                        )}
-                    </Button>
-                </form>
+                <Form
+                    setRequestState={setRequestState}
+                    setIsModalOpen={setIsModalOpen}
+                    loading={loading}
+                />
             </main>
         </>
     )
