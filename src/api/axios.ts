13a5da0c3d6ec8b5cd4022e6f8692f257778676b
Yuri Odilon <moura.n.yuri@gmail.com>
2025-07-14 16:00:35 -0400

    feat: dockerfile criado

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+const isDev = window.location.hostname === "localhost";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-    baseURL: apiUrl,
+    baseURL: isDev ? apiUrl : '/api',
     headers: {
         'Content-Type': 'application/json',
     }
